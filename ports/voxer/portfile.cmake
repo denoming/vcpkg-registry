bb8aee27f3ddf3082e65ea096a8cd0f54593d07c
Denys Asauliak <denoming@gmail.com>
2024-11-20 19:04:47 +0200

    update voxer to 0.2.3

--- ports/voxer/portfile.cmake
+++ ports/voxer/portfile.cmake
@@ -1,15 +1,17 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO denoming/voxer
-    REF v0.2.2
-    SHA512 fa105a9bc5d61d9c756012424aeecb3c07d68a6d2c43a71f24cdd83e9d978a55b7862a86fd2ac58388c280b2a5362472221f842e80f80a1863c7b8fdeeffb26b
+    REF v0.2.3
+    SHA512 faacfb75942abcc10cc407ee21be879b281492bd9b88a72e0d9ee3746ee80ac4fac3a6a789c69fd41e0d1336c49bf52cf6f819bfeb1af28e593771450044c4b6
     HEAD_REF main
 )
 
 vcpkg_configure_cmake(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS -DVOXER_ENABLE_TESTS=OFF
+            -DVOXER_ENABLE_CLI=OFF
             -DVOXER_ENABLE_FORMATTED=ON
+            -DVOXER_VOICES_DOWNLOAD=OFF
 )
 
 vcpkg_install_cmake()
